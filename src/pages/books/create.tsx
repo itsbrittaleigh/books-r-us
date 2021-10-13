828d831362bec59975180e064c1492b17fb74576
Britta Rodenbeck <cheers@brittarodenbeck.com>
2021-10-13 14:19:02 -0400

    :sparkles: adding multiple books at once

--- src/pages/books/create.tsx
+++ src/pages/books/create.tsx
@@ -1,81 +1,163 @@
 import React from 'react';
-import { useForm } from 'react-hook-form';
+import { useForm, useFieldArray } from 'react-hook-form';
 
-interface IFormInput {
-  author: string;
-  category: string;
-  inventory: number;
-  isbn: string;
-  notes: string;
-  title: string;
+type FormValues = {
+  books: {
+    author: string;
+    category: string;
+    inventory: number;
+    isbn: string;
+    notes: string;
+    title: string;
+    id: number;
+  }[];
 };
 
 const Create: React.FC = () => {
-  const { register, handleSubmit, formState: { errors } } = useForm<IFormInput>();
-  const books: Book[] = JSON.parse(localStorage.getItem('BRU_Books')!);
+  const books = JSON.parse(localStorage.getItem('BRU_Books')!);
+  const nextId = Math.max.apply(Math, books.map((book: Book) => book.id )) + 1;
 
-  const onSubmit = (data: IFormInput) => {
-    // get id by finding largest current ID and adding 1
-    const id = Math.max.apply(Math, books.map((book: Book) => book.id )) + 1;
+  const { control, formState: { errors }, handleSubmit, register } = useForm<FormValues>({
+    defaultValues: {
+      books: [{
+        author: '',
+        category: '',
+        inventory: 0,
+        isbn: '',
+        notes: '',
+        title: '',
+      }],
+    },
+    mode: 'onBlur',
+  });
+  const { fields, append, remove } = useFieldArray({
+    name: 'books',
+    control,
+  });
 
-    // add data to books array with new id
-    books.push({
-      ...data,
-      // inventory should be an integer, but HTML input treats it as a string
-      inventory: parseInt(data.inventory.toString(), 10),
-      id,
+  const onSubmit = (data: FormValues) => {
+    const booksToAdd = data.books;
+
+    booksToAdd.forEach((book, idx) => {
+      // convert inventories to numbers
+      book.inventory = parseInt(book.inventory.toString(), 10);
+      // add IDs
+      book.id = nextId + idx;
     });
 
-    // save to localStorage
+    books.push(...booksToAdd);
+
     localStorage.setItem('BRU_Books', JSON.stringify(books));
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div>
-        <label htmlFor="title">Title</label>
-        <input {...register('title', { required: true })} />
-        {errors.title && (
-          <span>This field is required</span>
-        )}
-      </div>
-      <div>
-        <label htmlFor="author">Author</label>
-        <input {...register('author', { required: true })} />
-        {errors.author && (
-          <span>This field is required</span>
-        )}
-      </div>
-      <div>
-        <label htmlFor="isbn">ISBN</label>
-        <input {...register('isbn', { pattern: /^[0-9]{10}$/i })} />
-        {errors.isbn && (
-          <span>The ISBN must be exactly ten digits.</span>
-        )}
-      </div>
-      <div>
-        <label htmlFor="inventory">Inventory</label>
-        <input
-          type="number"
-          {...register('inventory', { min: 0 })}
-        />
-        {errors.inventory && (
-          <span>You cannot have negative inventory.</span>
-        )}
-      </div>
-      <div>
-        <label htmlFor="category">Category</label>
-        <input {...register('category', { required: true })} />
-        {errors.category && (
-          <span>This field is required.</span>
-        )}
-      </div>
-      <div>
-        <label htmlFor="notes">Notes</label>
-        <textarea {...register('notes')} />
-      </div>
-      <button type="submit">Update</button>
-    </form>
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        {fields.map((field, index) => (
+          <div key={field.id}>
+            <section key={field.id}>
+              <div className={errors?.books?.[index]?.title ? 'form__field form__field--error' : 'form__field'}>
+                <label htmlFor={`books.${index}.title`}>Title</label>
+                <input
+                  {...register(
+                    `books.${index}.title` as const,
+                    {
+                      required: true,
+                    },
+                  )}
+                  type="text"
+                />
+                {errors?.books?.[index]?.title && (
+                  <span>This field is required.</span>
+                )}
+              </div>
+              <div className={errors?.books?.[index]?.author ? 'form__field form__field--error' : 'form__field'}>
+                <label htmlFor={`books.${index}.author`}>Author</label>
+                <input
+                  {...register(
+                    `books.${index}.author` as const,
+                    {
+                      required: true,
+                    }
+                  )}
+                  type="text"
+                />
+                {errors?.books?.[index]?.author && (
+                  <span>This field is required.</span>
+                )}
+              </div>
+              <div className={errors?.books?.[index]?.isbn ? 'form__field form__field--error' : 'form__field'}>
+                <label htmlFor={`books.${index}.isbn`}>ISBN</label>
+                <input
+                  {...register(
+                    `books.${index}.isbn` as const,
+                    {
+                      required: true,
+                      pattern: /^[0-9]{10}$/i,
+                    })}
+                  type="text"
+                />
+                {errors?.books?.[index]?.isbn && (
+                  <span>Your ISBN must be 10 digits.</span>
+                )}
+              </div>
+              <div className={errors?.books?.[index]?.inventory ? 'form__field form__field--error' : 'form__field'}>
+                <label htmlFor={`books.${index}.inventory`}>Inventory</label>
+                <input
+                  {...register(
+                    `books.${index}.inventory` as const,
+                    {
+                      required: true,
+                      min: 0,
+                    })}
+                  type="number"
+                />
+                {errors?.books?.[index]?.inventory && (
+                  <span>You cannot have negative inventory.</span>
+                )}
+              </div>
+              <div className={errors?.books?.[index]?.category ? 'form__field form__field--error' : 'form__field'}>
+                <label htmlFor={`books.${index}.category`}>Category</label>
+                <input
+                  {...register(
+                    `books.${index}.category` as const,
+                    {
+                      required: true,
+                    })}
+                  type="text"
+                />
+                {errors?.books?.[index]?.category && (
+                  <span>This field is required.</span>
+                )}
+              </div>
+              <div className="form__field">
+                <label htmlFor={`books.${index}.notes`}>Notes</label>
+                <textarea {...register(`books.${index}.notes` as const)} />
+              </div>
+              <button type="button" onClick={() => remove(index)}>
+                Delete book
+              </button>
+            </section>
+          </div>
+        ))}
+        <button
+          type="button"
+          onClick={() =>
+            append({
+              author: '',
+              category: '',
+              inventory: 0,
+              isbn: '',
+              notes: '',
+              title: '',
+            })
+          }
+        >
+          Add another book
+        </button>
+        <button type="submit">Create book{fields.length > 1 ? 's' : ''}</button>
+      </form>
+    </div>
   );
 };
 
